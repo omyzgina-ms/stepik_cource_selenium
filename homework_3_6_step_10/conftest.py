491bc1a4f9c5a0a4c674d78ab0478eb94f725554
omyzgina-ms <o.myzgina@melsoft-games.com>
2025-04-22 18:26:40 +0300

    method for choosing language is fixed

--- homework_3_6_step_10/conftest.py
+++ homework_3_6_step_10/conftest.py
@@ -1,17 +1,22 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default='en-gb',
                      help="Choose preferable language: en-gb, es, fr or ru")
 
-@pytest.fixture
-def language(request):
-    return request.config.getoption('--language')
-
 @pytest.fixture(scope="function")
 def browser(request):
-    browser = webdriver.Chrome()
+    language = request.config.getoption('--language')
+
+    # создаем список разрешенных языков и проверяем, входит ли переданное значение в этот список
+    allowed_languages = ['en-gb', 'es', 'fr', 'ar', 'ca', 'cs', 'da', 'de', 'el', 'fi', 'it', 'ko', 'nl', 'pl', 'pt', 'pt-br', 'ro', 'ru', 'sk', 'uk', 'zh-cn']
+    assert language in allowed_languages, f'{language} is not allowed language'
+
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    browser = webdriver.Chrome(options=options)
     yield browser
     print("\nquit browser..")
     browser.quit()
--- homework_3_6_step_10/test_items.py
+++ homework_3_6_step_10/test_items.py
@@ -3,21 +3,15 @@ import time
 
 class TestShopItems:
 
-    def test_buy_button_exists(self, browser, language):
-        #создаем список разрешенных языков и проверяем, входит ли переданное значение в этот список
-        allowed_languages = ['en-gb', 'es', 'fr', 'ar', 'ca', 'cs', 'da', 'de', 'el', 'fi', 'it', 'ko', 'nl', 'pl', 'pt', 'pt-br', 'ro', 'ru', 'sk', 'uk', 'zh-cn']
-        assert language in allowed_languages, f'{language} is not allowed language'
-
-        #получаем список страницы с указанным языком и переходим по ней
-        link = f'http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/'
+    def test_buy_button_exists(self, browser):
+        link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
         browser.get(link)
         time.sleep(10)
 
         #добавляем немного информации в лог
-        print(f'\nchosen language is {language}')
         page_language_list = browser.find_element(By.CSS_SELECTOR, "[selected='selected']")
         page_language = page_language_list.text
-        print(f'page language is {page_language}')
+        print(f'\npage language is {page_language}')
 
         #проверяем, что страница товара содержит кнопку добавления в корзину
         buy_button = browser.find_elements(By.CSS_SELECTOR, 'button.btn-add-to-basket')
