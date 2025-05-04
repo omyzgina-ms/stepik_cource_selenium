2c0fa1eac34bd493dcc65bf1f9982da5b7618fa3
omyzgina-ms <o.myzgina@melsoft-games.com>
2025-05-04 19:00:40 +0300

    product_page and its test added

--- module4/pages/locators.py
+++ module4/pages/locators.py
@@ -6,3 +6,10 @@ class MainPageLocators:
 class LoginPageLocators:
     LOG_IN_FORM = (By.CSS_SELECTOR, '#login_form')
     REGISTRATION_FORM = (By.CSS_SELECTOR, '#register_form')
+
+class ProductPageLocators:
+    BUY_BUTTON = (By.CSS_SELECTOR, 'button.btn-add-to-basket')
+    PRODUCT_NAME = (By.CSS_SELECTOR, '.product_main h1')
+    PRODUCT_PRICE = (By.CSS_SELECTOR, '.product_main .price_color')
+    PRODUCT_IN_BASKET = (By.CSS_SELECTOR, 'div#messages div.alert-success div.alertinner strong')
+    PRICE_IN_BASKET = (By.CSS_SELECTOR, 'div#messages div.alert-info div.alertinner p strong')
---
+++ module4/pages/product_page.py
@@ -0,0 +1,17 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+    def buy_product(self):
+        buy_button = self.browser.find_element(*ProductPageLocators.BUY_BUTTON)
+        buy_button.click()
+
+    def should_be_purchase_confirmation(self):
+        product_on_page = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME)
+        product_in_basket = self.browser.find_element(*ProductPageLocators.PRODUCT_IN_BASKET)
+        assert product_in_basket.text == product_on_page.text, f'Product in the basket does not match the product on the page. Got "{product_in_basket.text}" instead of "{product_on_page.text}"'
+
+    def should_be_basket_price(self):
+        price_on_page = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE)
+        price_in_basket = self.browser.find_element(*ProductPageLocators.PRICE_IN_BASKET)
+        assert price_on_page.text == price_in_basket.text, f'Price in the basket does not match the price on the page. Got "{price_in_basket.text}" instead of "{price_on_page.text}"'
