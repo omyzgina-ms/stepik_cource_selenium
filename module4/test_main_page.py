824b05c1178dc34af11520c52778ce5d6594bb62
omyzgina-ms <o.myzgina@melsoft-games.com>
2025-05-04 14:57:44 +0300

    locators config added

--- module4/test_main_page.py
+++ module4/test_main_page.py
@@ -2,15 +2,15 @@ from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
 import time
 
+link = 'http://selenium1py.pythonanywhere.com/'
+
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
-    time.sleep(10)
+    time.sleep(2)
 
 def test_guest_should_be_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
