7cd6de07e96e649ea560c42172d76fae433b237b
omyzgina-ms <o.myzgina@melsoft-games.com>
2025-05-04 14:30:15 +0300

    go_to_login_page test added

--- module4/test_main_page.py
+++ module4/test_main_page.py
@@ -1,16 +1,11 @@
 from selenium.webdriver.common.by import By
+from .pages.main_page import MainPage
 import time
 
-link = "http://selenium1py.pythonanywhere.com/"
-
-class TestMainPage:
-    @staticmethod
-    def go_to_login_page(browser):
-        login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click()
-
-    def test_guest_can_go_to_login_page(self, browser):
-        browser.get(link)
-        self.go_to_login_page(browser)
-        time.sleep(10)
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    time.sleep(10)
 
